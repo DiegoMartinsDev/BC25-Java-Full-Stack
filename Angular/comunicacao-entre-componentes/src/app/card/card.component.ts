9b54e494189c876541931768f65b2fa7934fe40a
Mohotrisk <diegosocial20@hotmail.com>
2022-10-19 23:32:41 -0300

    comunicação entre componentes

---
+++ Angular/comunicacao-entre-componentes/src/app/card/card.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+
+  @Input() 
+  banner: string = 'https://images.unsplash.com/file-1636585210491-f28ca34ea8ecimage?dpr=2&auto=format&fit=crop&w=416&q=60'
+
+  @Input()
+  titulo: string = 'Lorem ipsum dolor sit amet consectetur '
+
+  @Input()
+  username: string = 'Diego Martins'
+
+  @Input()
+  userFoto: string = 'https://avatars.githubusercontent.com/u/95890180?v=4'
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
