c12c72d1777ce5fe9b1d7564bd1100d8f680dc85
Mohotrisk <diegosocial20@hotmail.com>
2022-10-30 22:54:09 -0300

    Exercício Bichinho

---
+++ Java/estudojava/src/main/java/aulas/poo/Bichinho.java
@@ -0,0 +1,17 @@
+package aulas.poo;
+
+public class Bichinho {
+   public String nome;
+    public double idade;
+    public double peso;
+    public String bucho;
+    public boolean estaDormindo;
+}
+Bichinho(String nome, double idade,double peso,String bucho,boolean estaDormindo)
+{
+    this.nome = nome;
+    this.idade = idade;
+    this.peso = peso;
+    this.bucho = bucho;
+    this.estaDormindo = false;
+}
