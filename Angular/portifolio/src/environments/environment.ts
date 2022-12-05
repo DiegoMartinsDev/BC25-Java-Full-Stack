b63ea1eab7e0100969867ff993432f2b0216e0ee
Mohotrisk <diegosocial20@hotmail.com>
2022-12-05 20:55:20 -0300

    Atualização de repositórios

---
+++ Angular/portifolio/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyB72bR1zdPcmPduFZ-XxpkrkUYdTf7oJ74",
+    authDomain: "portifolio-c771c.firebaseapp.com",
+    projectId: "portifolio-c771c",
+    storageBucket: "portifolio-c771c.appspot.com",
+    messagingSenderId: "683054362679",
+    appId: "1:683054362679:web:9e25b8fe6154c1ce590fb2"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
